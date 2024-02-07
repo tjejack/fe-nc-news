24bb489db175bc77eaedbb2f60846782fb0a97ae
tjejack <tjeidanjack@gmail.com>
2024-02-07 11:49:39 +0000

    hid nonfunctional placeholder link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ export default function NavBar() {
   return (
     <div id="navigation-bar">
       <Link to='/' className="nav-button">Home</Link>
-      <Link to='/login/' className="nav-button">Change User</Link>
+      {/* <Link to='/login/' className="nav-button">Change User</Link> */}
     </div>
   );
 }
