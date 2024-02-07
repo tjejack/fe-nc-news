7e83443f452fd8c346155cd82acd8eb4c39fb609
tjejack <tjeidanjack@gmail.com>
2024-02-07 11:49:10 +0000

    added functionality for user's latest comment to be posted to the top of the comments list

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import getCommentsByArticleId from "../../utils/getCommentsByArticleId.js";
-import CommentCard from "./CommentCard.jsx"
+import CommentCard from "./CommentCard.jsx";
 
-export default function CommentsList({ article_id }) {
+export default function CommentsList({ article_id, latestUserComment }) {
   const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
@@ -15,19 +15,39 @@ export default function CommentsList({ article_id }) {
       .catch((error) => {
         setIsError(true);
       });
-  }, []);
+  }, [latestUserComment]);
   if (isLoading) {
     return <p>Loading comments...</p>;
   }
   if (isError) {
     return <p>Something went wrong!</p>;
   }
+  if (latestUserComment) {
+    return (
+      <section className="comments-list">
+        <CommentCard
+          key={latestUserComment.comment_id}
+          comment={latestUserComment}
+        />
+        {comments.map((comment) => {
+          {
+            return comment.comment_id ===
+              latestUserComment.comment_id ? null : (
+              <CommentCard key={comment.comment_id} comment={{ ...comment }} />
+            );
+          }
+        })}
+      </section>
+    );
+  }
   return (
     <section className="comments-list">
       {comments.map((comment) => {
-        return (
-          <CommentCard key={comment.comment_id} comment={{ ...comment }} />
-        );
+        {
+          return (
+            <CommentCard key={comment.comment_id} comment={{ ...comment }} />
+          );
+        }
       })}
     </section>
   );
