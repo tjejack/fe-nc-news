ecda7eca56a86e8743fd99547906c94c4dada21b
tjejack <tjeidanjack@gmail.com>
2024-02-07 11:46:26 +0000

    added functionality of display name and avatar

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
+import getUserByUsername from "../../utils/getUserByUsername.js";
+
 export default function CommentCard({ comment }) {
+  const [commentUser, setCommentUser] = useState({});
   const published = new Date(comment.created_at);
   const displayTime = published.toUTCString();
+  useEffect(()=>{
+    getUserByUsername(comment.author).then((commentAuthorAccount) => {
+      setCommentUser({...commentAuthorAccount});
+    })
+  }, [])
   return (
     <div className="comment-card-extra-height">
       <div className="comment-card" xs={6} md={6} lg={6}>
-        <header className="comment-author">{comment.author}</header>
+        <div className="comment-author-container">
+        <img className="comment-avatar" src={commentUser.avatar_url} />
+        <header className="comment-author">{commentUser.name}</header>
+        </div>
         <p className="comment-published">{displayTime}</p>
         <p className="comment-body">{comment.body}</p>
         <p className="comment-votes">Votes: {comment.votes}</p>
