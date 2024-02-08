d4cdd9111d3e352da1ad4ed2b112dbd519813085
tjejack <tjeidanjack@gmail.com>
2024-02-08 11:01:43 +0000

    added links to login page and avatar of current user

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../contexts/User";
 
 export default function NavBar() {
+  const { currentUser } = useContext(UserContext);
   return (
-    <div id="navigation-bar">
-      <Link to='/' className="nav-button">Home</Link>
-      {/* <Link to='/login/' className="nav-button">Change User</Link> */}
+    <div id="nav-holder">
+      <div id="navigation-bar">
+        <Link to="/" className="nav-button">
+          Home
+        </Link>
+        <Link to="/login/" className="nav-button">
+          Change User
+        </Link>
+      </div>
+        <Link to="/login/">
+          <img id="current-user-avatar" src={currentUser.avatar_url} />
+        </Link>
     </div>
   );
 }
