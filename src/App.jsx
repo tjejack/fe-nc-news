ead90a06f6a33b9f3f05481ea7601bec16faa887
tjejack <tjeidanjack@gmail.com>
2024-02-09 10:33:25 +0000

    routing for 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import NavBar from './components/NavBar';
-import Home from './components/Home';
-import ArticlePage from './components/ArticlePage';
-import Login from './components/Login';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import ArticlePage from "./components/ArticlePage";
+import Login from "./components/Login";
+import NotFound from "./components/NotFound";
 
 function App() {
-  return (<div id="main-body">
-  <Header />
-  <NavBar />
-  <Routes>
-    <Route path='/'
-    element={
-      <Home />
-    } />
-    <Route path='/article/:article_id'
-    element={<ArticlePage />}
-    />
-        <Route path='/login'
-    element={<Login />}
-    />
-  </Routes>
-  </div>
-  )
+  return (
+    <div id="main-body">
+      <Header />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/article/:article_id" element={<ArticlePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
